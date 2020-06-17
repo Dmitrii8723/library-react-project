12d065a2f782359b16adf1234827114e5f864a9a
Dmitrii Kharapaev <dmitrii@birdi.com.au>
2020-06-18 00:20:29 +1000

    Changed colors

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Sidebar from '../common/Sidebar';
 
 const Dashboard = (props) => (
-<div className="flex flex-grow 1 bg-gray-400">
+<div className="flex flex-grow 1 bg-white">
 <Sidebar />
-<div className="bg-red-400 flex-grow-1 w-3/4">
+<div className="bg-gray-400 flex-grow-1 w-3/4">
 </div>
 </div>
 );
--- src/infrastructure/index.jsx
+++ src/infrastructure/index.jsx
@@ -4,7 +4,7 @@ import Sidebar from '../common/Sidebar';
 const Infrastructure = (props) => (
 <div className="flex flex-grow 1 bg-green-400">
 <Sidebar />
-<div className="bg-red-400 flex-grow-1 w-3/4">
+<div className="bg-gray-400 flex-grow-1 w-3/4">
 </div>
 </div>
 );
